91793c5c253e85ef303970d1da8d9a973d3598c9
Utku <34550887+utku-guclu@users.noreply.github.com>
2023-12-31 16:05:44 +0300

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,25 @@ const runner            = require('./test-runner');
 
 const app = express();
 
+// mongoose.js
+
+const mongoose = require('mongoose');
+
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/your-database-name';
+
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.use(cors({origin: '*'})); //USED FOR FCC TESTING PURPOSES ONLY!
