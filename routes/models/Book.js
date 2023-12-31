efb04b559d91a333ed832f0af1a8d7ebf5132cb9
Utku <34550887+utku-guclu@users.noreply.github.com>
2023-12-31 16:06:17 +0300

    Create Book.js

---
+++ routes/models/Book.js
@@ -0,0 +1,18 @@
+// models/Book.js
+
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  comments: {
+    type: [String],
+    default: [],
+  },
+});
+
+const Book = mongoose.model('Book', bookSchema);
+
+module.exports = Book;
